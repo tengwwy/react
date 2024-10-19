57669933a7dac8e8f2cf7a0b6d0daf35d155970e
Teng <tengjiang32@gmail.com>
2024-10-19 16:43:56 -0400

    adding the code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Greeting</h1>
+      <p>Hello World</p>
+      <button>CLICK ME</button>
+      <a>LINK</a>
+    </div>
+  )
+}
+export default App;
